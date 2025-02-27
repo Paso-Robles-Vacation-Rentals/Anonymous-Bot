61d2ddfd8bd4702ef1e6dbc9148468bec98781f8
Anthony DeGarimore <Anthony@DeGarimore.com>
2025-02-26 23:44:19 -0800

    Update license information to GNU Affero General Public License.
    The changes in this commit update the license information in the main.py file from the GNU General Public License to the GNU Affero General Public License.

--- main.py
+++ main.py
@@ -2,18 +2,18 @@
 Anonymous Bot: A Slack app that handles anonymous messaging.
 Copyright (C) 2025 Paso Robles Vacation Rentals
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0-standalone.html.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import json
